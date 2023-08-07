bd691b1614e26961d6ba8cd76a31c8d5add924bb
Rifqi Habibie <rifqim832@gmail.com>
2023-08-07 22:33:27 +0700

    Habibie 07-08-2023

--- Tailwind/bagian 1/tailwind.config.js
+++ Tailwind/bagian 1/tailwind.config.js
@@ -5,23 +5,5 @@ module.exports = {
         extend: {},
     },
     plugins: [],
-};
-
-module.exports = {
-    theme: {
-        extend: {
-            spacing: {
-                13: "3.25rem",
-                15: "3.75rem",
-                128: "32rem",
-                144: "36rem",
-            },
-            fontFamily: {
-                inter: ["Inter"],
-            },
-            colors: {
-                ternary: "#c0ffee",
-            },
-        },
-    },
+    darkMode: "class",
 };
