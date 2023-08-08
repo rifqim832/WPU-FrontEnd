ca24d2dd0cd5b9c214aed7bdd3ebed756c07631d
Rifqi Habibie <rifqim832@gmail.com>
2023-08-08 15:07:17 +0700

    Habibie 08-08-2023

--- Tailwind/bagian 1/tailwind.config.js
+++ Tailwind/bagian 1/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                "spin-slow": "spin 3s linear infinite",
+                goyang: "goyang 1s ease-in-out infinite",
+            },
+            keyframes: {
+                goyang: {
+                    "0%, 100%": { transform: "rotate(-3deg)" },
+                    "50%": { transform: "rotate(3deg)" },
+                },
+            },
+        },
     },
     plugins: [],
     darkMode: "class",
