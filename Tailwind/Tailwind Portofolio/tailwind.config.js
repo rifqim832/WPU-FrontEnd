1c4f1bfd2c0a2e1764cae303afac53499da04735
Rifqi Habibie <rifqim832@gmail.com>
2023-08-27 19:16:01 +0700

    27

---
+++ Tailwind/Tailwind Portofolio/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#f43f5e',
+        dark: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+};
+
